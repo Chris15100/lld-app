46765880fe56e282297f5e4fb75916ed16d61e84
Christopher Gallo <christophergallo@android.home>
2025-08-03 17:44:48 +0200

    Mise à jour de l'application

--- pages/3_Normes_page.py
+++ pages/3_Normes_page.py
@@ -2,7 +2,7 @@ import streamlit as st
 import base64
 
 # Chemin vers ton image
-image_path = "/Users/christophergallo/Desktop/Application perso/Logo/Doc1-1.png"
+image_path = "images/logo.png"
 
 # Lire l'image en base64
 def get_base64_of_bin_file(bin_file):
